7193d47883f1611e7271454d60aba6e7ebac230f
Gunther Cox <gunthercx@gmail.com>
2013-09-18 06:54:24 -0400

    Create Neuron.h

---
+++ Arduino_Neuron/Neuron.h
@@ -0,0 +1,37 @@
+//#ifndef Neuron_h
+//#define Neuron_h
+
+#include <Arduino.h>
+
+class Neuron {
+
+public:
+ Neuron(int analog, int digital);
+
+ // DENDRITES AND SYNAPTIC TERMINALS
+ int analogPin;
+ int digitalPin;
+ 
+ // DENDRITE READING WHEN TERMINALS SET TO LOW OR HIGH
+ int calibratedAmountLow;
+ int calibratedAmountHigh;
+ 
+ // A current flaw is that the action potential does not regulate over time.
+ // Neurotransmitters naturally ware off over time.
+ int actionPotential;
+
+ void calibrate();
+
+ boolean actionPotentialProbability(int excitory, int inhibitory);
+ 
+ int signalType();
+ long getFrequency(int pin);
+
+ void excitory();
+ void inhibitory();
+ 
+ // RETURN NEURON PINS
+ int get_dendrite();
+ int get_terminal();
+
+};
