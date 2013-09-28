88ba5807898136570b9101c376b33fd3d132fc9b
Gunther Cox <gunthercx@gmail.com>
2013-09-28 07:16:50 -0400

    Update Neuron.cpp

--- Arduino_Neuron/Neuron.cpp
+++ Arduino_Neuron/Neuron.cpp
@@ -9,20 +9,22 @@ Neuron::Neuron(int analog, int digital) {
 
 // CALIBRATE NEURON DENDRITES
 void Neuron::calibrate() {
+  
+  // SET TERMINAL HIGH
+  digitalWrite(digitalPin, HIGH);
+
+  // RECORD HIGH READING
+  calibratedAmountHigh = analogRead(analogPin);
+  
+  delay(neuronSpeed);
 
   // SET TERMINAL LOW
   digitalWrite(digitalPin, LOW);
 
   // RECORD LOW READING
   calibratedAmountLow = analogRead(analogPin);
-
-  // SET TERMINAL HIGH
-  digitalWrite(digitalPin, HIGH);
-
-  // RECORD HIGH READING
-  calibratedAmountHigh = analogRead(analogPin);
-
-  Serial.println("Neuron calibrated");
+  
+  Serial.println("Neuron calibrated: " + String(calibratedAmountLow) + " - " + String(calibratedAmountHigh));
 }
 
 // RECIEVE A SIGNAL FROM ANOTHER NEURON
@@ -36,15 +38,19 @@ int Neuron::signalType() {
   long frequency = getFrequency(analogPin);
   
   // EXCITORY FREQUENCY
-  if (frequency > 400) {
+  if (frequency > neuronSpeed) {
+    Serial.println("Excitory " + frequency);
     return 1;
   } 
   
   // INHIBITORY FREQUENCY
-  if (frequency < 400 && frequency > 200) {
+  // THE 10 IS A BUFFER FOR ANY OUTSIDE INTERFERENCE
+  if (frequency < (neuronSpeed * 2) && frequency > 10) {
+    Serial.println("Inhibitory " + frequency);
     return 2;
   }
     // NO SIGNAL
+    Serial.println("No signal " + frequency);
     return 0;
 
 }
@@ -52,26 +58,28 @@ int Neuron::signalType() {
 // GET THE FREQUENCY OF THE INCOMING SIGNAL
 // http://tushev.org/articles/arduino/item/51-measuring-frequency-with-arduino
 long Neuron::getFrequency(int pin) {
-  #define SAMPLES 4096
+  int SAMPLES = 10;
   long freq = 0;
-  for(unsigned int j=0; j<SAMPLES; j++) freq+= 500000/pulseIn(pin, HIGH, 250000);
+  for(unsigned int j=0; j<SAMPLES; j++) freq+= 50/pulseIn(pin, HIGH, 250);
   return freq / SAMPLES;
 }
 
 // SENDS AN EXCITORY SIGNAL
 void Neuron::excitory() {
-  digitalWrite(digitalPin, HIGH);
-  delay(250);
-  digitalWrite(digitalPin, LOW);
-  delay(250);
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(digitalPin, HIGH);
+    delay(neuronSpeed);
+    digitalWrite(digitalPin, LOW);
+    delay(neuronSpeed);
+  }
 }
 
 // SENDS AN INHIBITORY SIGNAL
 void Neuron::inhibitory() {
   digitalWrite(digitalPin, HIGH);
-  delay(250 * 2);
+  delay(neuronSpeed * 2);
   digitalWrite(digitalPin, LOW);
-  delay(250);
+  delay(neuronSpeed);
 }
 
 // CONTROLS A NEURONS ACTION POTENTIAL
